b093a65f7b445fee745e7647b0683f7ec64eac05
Davit Markarian <davit@udxs.me>
2022-04-17 22:43:13 -0700

    Add module and its tb

---
+++ rtl/fib.sv
@@ -0,0 +1,56 @@
+module fibonacci_calculator (
+	input  [4:0] input_s, 
+	input  reset,
+	input  begin_fibo, 
+	input  clk, 
+	output logic done, 
+	output logic[15:0] fibo_out);   
+
+	enum {
+		RESET,
+		WORKING,
+		DONE
+	} fib_state;
+
+	reg [$bits(fib_state)-1:0] state;
+	reg [15:0] n;
+	reg [15:0] n_prev;
+	wire [15:0] n_next;
+	reg [4:0] ctr;
+	assign n_next = n + n_prev;
+
+	assign fibo_out = n;
+	assign done = state == DONE;
+
+	always_ff @(posedge clk) begin
+		unique case (state)
+			RESET: begin
+				if(begin_fibo) begin
+					if(input_s == 5'd1 || input_s == 5'd2) begin
+						state <= DONE;
+						n <= 16'd1;
+					end else begin
+						state <= WORKING;
+						ctr <= input_s - 5'd2;
+					end
+				end
+			end
+			WORKING: begin
+				ctr <= ctr - 1;
+				n_prev <= n;
+				n <= n_next;
+				if(ctr == 0)
+					state <= DONE;
+			end
+		endcase
+
+				
+		
+		if(reset) begin
+			n <= 1;
+			n_prev <= 1;
+			state <= RESET;
+		end
+	end
+	
+endmodule
---
+++ rtl/fib_tb.sv
@@ -0,0 +1,68 @@
+module fibonacci_tb ();
+    reg [15:0] fib_tab [24];
+
+    reg [4:0] input_s;
+    reg reset;
+    reg begin_fibo;
+    reg clk;
+    wire done;
+    wire [15:0] fibo_out;
+
+    fibonacci_calculator dut (
+	    input_s, 
+	    reset,
+	    begin_fibo, 
+	    clk, 
+	    done, 
+	    fibo_out);  
+
+    integer tab_file;
+    integer i;
+    integer clock_ctr;
+
+    initial begin
+        #1 clk = 0;
+
+        $display ("Fibonacci Testbench");
+        $display ("D. Markarian, 2022");
+        $display("======");
+
+        tab_file = $fopen("table.txt");
+        for(i = 0; i < 24; i++) begin
+            $fscanf(tab_file, "%d", fib_tab[i]);
+        end
+        for (i = 0; i < 24; i++) begin
+
+            #1 clk = 1;
+            #1 clk = 0;
+            reset = 1;
+            #1 clk = 1;
+            #1 clk = 0;
+            reset = 0;
+
+            input_s = i[4:0];
+            begin_fibo = 1;
+
+            #1 clk = 1;
+            #1 clk = 0;
+            
+            begin_fibo = 0;
+
+            clock_ctr = 1;
+            while(!done) begin
+                #1 clk = 1;
+                #1 clk = 0;        
+                clock_ctr = clock_ctr + 1;
+            end
+
+            $display ("%d-th Fibonacci: E(%d) ?= %d. t = %d cycles.", i, fib_tab[i], fibo_out, clock_ctr);
+            if(fib_tab[i] ==  fibo_out)
+                $display("\t- PASS");
+            else
+                $display("\t- FAIL");
+        end
+
+        $display("======");
+    end
+
+endmodule
