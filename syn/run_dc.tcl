388d0134ec1cdc667757316d211181f488ba0606
Davit Markarian <davit@udxs.me>
2022-04-17 23:22:19 -0700

    Fix syn paths

--- syn/run_dc.tcl
+++ syn/run_dc.tcl
@@ -44,8 +44,8 @@ set verilogout_single_bit false
 set_host_options -max_cores 6
 
 # read RTL
-set rtl_files [glob $rtlPath/*.v] [glob $rtlPath/*.sv]
-foreach fi [lsearch -glob -all $rtl_files $rtlPath/*_tb.v] {set rtl_files [lset rtl_files $fi ""]}
+set rtl_files glob $rtlPath/*.sv]
+foreach fi [lsearch -glob -all $rtl_files $rtlPath/*_tb.sv] {set rtl_files [lset rtl_files $fi ""]}
 analyze -format verilog -lib WORK $rtl_files
 
 elaborate $top_module -lib WORK -update
